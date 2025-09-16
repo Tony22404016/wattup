a71a21a1c62ad472445793bd4e5a515111796f20
Junaidi <junaialberto001@gmail.com>
2025-09-16 11:35:33 +0800

    setu up halaman home page dan menambahkan form login dan register

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+    <link rel="stylesheet" href="Login.css">
+</head>
+<body>
+    <section class="container">
+        <div class="login-form">
+            <h2>Hallo, Wellcome Back</h2>
+            <p class="welcome-text">Wellcome back to your special place</p>
+            
+            <div class="input-group">
+                <input type="text" placeholder="Enter username">
+            </div>
+            
+            <div class="input-group">
+                <input type="password" placeholder="Enter password">
+            </div>
+            
+            <div class="remember-me">
+                <input type="checkbox" id="remember">
+                <label for="remember">Remember me</label>
+            </div>
+            
+            <button class="btn-signin">Sign in</button>
+            
+            <div class="signup-link">
+                <p>Don't have an account? <a href="#">Sign up</a></p>
+            </div>
+        </div>
+        
+        <div class="login-image">
+            <!-- Ganti dengan URL gambar Anda sendiri -->
+            <img src="asset/Login_Image.png" alt="Login Image">
+        </div>
+    </section>
+</body>
+</html>
---
+++ resources/views/Sign_Up.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Regis</title>
+    <link rel="stylesheet" href="Login.css">
+</head>
+<body>
+    <section class="container">
+        <div class="login-form">
+            <h2>Get Started with Us</h2>
+            <p class="welcome-text">Register now and be part of us</p>
+            
+            <div class="input-group">
+                <input type="text" placeholder="Enter username">
+            </div>
+            
+            <div class="input-group">
+                <input type="password" placeholder="Enter password">
+            </div>
+            
+            <div class="remember-me">
+                <input type="checkbox" id="remember">
+                <label for="remember">Remember me</label>
+            </div>
+            
+            <button class="btn-signin">Sign up</button>
+            
+            <div class="signup-link">
+                <p>Already have an account? <a href="#">Sign in</a></p>
+            </div>
+        </div>
+        
+        <div class="login-image">
+            <!-- Ganti dengan URL gambar Anda sendiri -->
+            <img src="asset/Login_Image.png" alt="Login Image">
+        </div>
+    </section>
+</body>
+</html>
